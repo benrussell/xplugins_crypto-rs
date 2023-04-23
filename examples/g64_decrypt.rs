892e078f8e9ad1e018552e16da1af9bf5b2513a8
Ben Russell <br@x-plugins.com>
2023-04-23 17:46:25 +1000

    Core lib.rs is now capable of decoding encrypted data. Does not verify HMAC!

--- examples/g64_decrypt.rs
+++ examples/g64_decrypt.rs
@@ -6,8 +6,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();    
     let filename = &args[1];
     
-    let plain_text = xplugins_crypto::decrypt_file(filename).expect("Decrypt failed.");
+    let plain_text = xplugins_crypto::decrypt_file(filename, true).expect("Decrypt failed.");
 
-    println!("plain_text:\n{:?}", plain_text)
+    //println!("plain_text:\n{:?}", plain_text)
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 
+use libaes::Cipher;
+//use sha256;
+use sha2;
+use sha2::Digest;
+
 // File structure should be:
 // G64000
 // Init Vector 128bit (16 bytes)
@@ -7,7 +12,27 @@
 // 256 byte RSa signature optional
 
 
-fn check_header( data: Vec<u8> ) -> Result<String, String>{
+fn get_key_hash( raw_key: &str ) -> [u8; 16]{
+
+    println!("raw key: {}", raw_key);
+
+    let hash = sha2::Sha256::digest( raw_key );
+    let hash_16 = &hash[0..16];
+
+    println!("   hash: {:x}", hash);
+    println!("hash_16: {:?}", hash_16);
+    
+    let mut ret: [u8; 16] = [0; 16];
+    for i in 0..16{
+        ret[i] = hash_16[i];
+    }
+
+    ret
+
+}
+
+
+fn check_header( data: &Vec<u8> ) -> Result<String, String>{
     let header = String::from_utf8(data[0..6].to_vec()).expect("Unable to convert header to string.");
     if header != "G64000" {
         return Err(format!("Invalid header: {}", header));
@@ -16,19 +41,17 @@ fn check_header( data: Vec<u8> ) -> Result<String, String>{
 }
 
 
-fn get_iv( data: Vec<u8> ) -> Result<Vec<u8>, String>{
-    let iv = data[6..22].to_vec();
-    Ok(iv)
+fn get_iv( data: &Vec<u8> ) -> &[u8] {
+    &data[6..22]
 }
 
 
-fn get_hmac( data: Vec<u8> ) -> Result<Vec<u8>, String>{
-    let hmac = data[22..54].to_vec();
-    Ok(hmac)
+fn get_hmac( data: &Vec<u8> ) -> &[u8] {
+    &data[22..54]    
 }
 
 
-pub fn decrypt_file( filename: &str ) -> Result<String, String>{
+pub fn decrypt_file( filename: &str, signed_file: bool ) -> Result<String, String>{
 
     use std::io::Read;
 
@@ -40,23 +63,86 @@ pub fn decrypt_file( filename: &str ) -> Result<String, String>{
     fh.read_to_end(&mut data).expect(&file_read_err);
     println!("read {} bytes of encrypted data.", data.len());
 
-    println!("check header: {:?}", check_header(data.clone()) );
+    const HEADER_LEN_G64: usize = 6;
+    const HEADER_LEN_IV: usize = 16;
+    const HEADER_LEN_HMAC: usize = 32;
+    const HEADER_LEN_BLOB: usize = HEADER_LEN_G64 + HEADER_LEN_IV + HEADER_LEN_HMAC;
 
-    let iv = get_iv(data.clone()).expect("Unable to get IV.");
+    if data.len() < HEADER_LEN_BLOB {
+        return Err(format!("File too short to be a valid G64 file: {}", filename));
+    }
+
+    let mut data_blob = data.split_off( HEADER_LEN_BLOB );
+    println!("Checking header: {:?}", check_header(&data) );
+
+    let iv = get_iv(&data);
     println!("  iv: {:?}", iv);
 
-    let hmac = get_hmac(data.clone()).expect("Unable to get HMAC.");
+    let hmac = get_hmac(&data);
     println!("hmac: {:?}", hmac);
 
-    let payload_offset = 6 + 16 + 32;
-    let data = data.split_off(payload_offset);
 
-    for b in data.iter(){
-        print!("{:x} ", b);
+
+    if signed_file {
+        println!("Capturing RSA signature.");
+        let _rsa_sig = data_blob.split_off( data_blob.len() - 256 );
+
+        //println!("rsa_sig: {:?}", rsa_sig);
     }
-    //println!("data: {:x}", data);
 
-    Ok("fubar".to_string())
+
+    const PRINT_BLOB: bool = true;
+    if PRINT_BLOB {
+        //print a hex dump of the data segment
+        println!("AES Payload:");
+        let mut counter_newline = 0;
+        let mut counter_space = 0;
+        for b in data_blob.iter(){
+            print!("{:02x}", b);
+
+
+            counter_space += 1;
+            if counter_space == 4 {
+                print!(" ");
+                counter_space = 0;
+            }
+
+
+            counter_newline += 1;
+            if counter_newline >= 36 {
+                println!("");
+                counter_newline = 0;
+                counter_space = 0;
+            }
+        
+            
+        }
+    }
+    
+    println!("");
+
+
+    let readable_password = "good-luck-cracking-rsa-sigs-you-pathetic-idiots";
+
+    let key = get_key_hash(readable_password);
+    for b in key.iter(){
+        print!("{:02x}", b);
+    }
+    println!("");
+
+    let cipher = Cipher::new_128(&key);
+
+    let decrypted = cipher.cbc_decrypt(iv, &data_blob[..]);
+
+
+    let dec_str = String::from_utf8(decrypted).expect("Unable to convert decrypted data to string.");
+    println!("[{}]", dec_str);
+
+    //println!("{:?}", decrypted);
+
+
+    
+    Ok("no_decrypt".to_string())
 }
 
 
