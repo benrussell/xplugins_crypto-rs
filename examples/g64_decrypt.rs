c1aad69613a6b70c629bb301b30d60ad52cfde2e
Ben Russell <br@x-plugins.com>
2023-04-23 18:00:58 +1000

    password input and plain text print moved to example host

--- examples/g64_decrypt.rs
+++ examples/g64_decrypt.rs
@@ -6,8 +6,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();    
     let filename = &args[1];
     
-    let plain_text = xplugins_crypto::decrypt_file(filename, true).expect("Decrypt failed.");
+    let password = "good-luck-cracking-rsa-sigs-you-pathetic-idiots";
+    
+    let plain_text = xplugins_crypto::decrypt_file(filename, true, password).expect("Decrypt failed.");
 
-    //println!("plain_text:\n{:?}", plain_text)
+    println!("plain_text:\n{}", plain_text)
 
 }
