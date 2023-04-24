976fd84a5645173b3b7a37f437b1bf5e381b122a
Ben Russell <br@x-plugins.com>
2023-04-25 00:26:06 +1000

    Imports RSA sig code and keystore data.

---
+++ src/rsa.rs
@@ -0,0 +1,34 @@
+
+// Use the ring::signature to verify a sha1 signature against a public key
+use ring::signature::{self, UnparsedPublicKey};
+
+#[derive(Debug)]
+struct SignatureError(String);
+
+impl std::fmt::Display for SignatureError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "RSA Signature Error: {}", self.0)
+    }
+}
+
+impl std::error::Error for SignatureError {}
+
+pub fn verify_signature(
+    public_key: &[u8],
+    signature: &[u8],
+    message: &[u8],
+) -> Result<String, Box<dyn std::error::Error>> {
+    let public_key =
+        //UnparsedPublicKey::new(&signature::RSA_PKCS1_2048_8192_SHA256, public_key);
+        UnparsedPublicKey::new(&signature::RSA_PKCS1_2048_8192_SHA1_FOR_LEGACY_USE_ONLY, public_key);
+
+    let verified = public_key
+        .verify(message, signature)
+        .map_err(|_| "Invalid Signature".to_string());
+
+    match verified {
+        Ok(_) => Ok("Signature verified.".to_string()),
+        Err(_e) => Err(Box::new(SignatureError("Invalid Signature.".to_string()))),
+    }
+}
+
