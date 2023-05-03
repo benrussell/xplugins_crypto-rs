037b76efe54436320938111372e6ae8a114fb695
Ben Russell <br@x-plugins.com>
2023-05-04 05:38:07 +1000

    examples/g64_decrypt updated to new fn sig

--- examples/g64_decrypt.rs
+++ examples/g64_decrypt.rs
@@ -1,15 +1,18 @@
 
-use xplugins_crypto;
+use xplugins_crypto::{self, g64::DecryptOptions};
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();    
-    let filename = &args[1];
     
-    let password = "good-luck-cracking-rsa-sigs-you-pathetic-idiots";
-    
-    let plain_text = xplugins_crypto::g64::decrypt_file(filename, true, password).expect("Decrypt Failed");
+    let decrypt_options = DecryptOptions{
+        filename: args[1].clone(),
+        password: "good-luck-cracking-rsa-sigs-you-pathetic-idiots".to_string(),
+        signed_file: true,
+    };
 
-    println!("plain_text:\n{}", plain_text)
+    let plain_text = xplugins_crypto::g64::decrypt_file(decrypt_options).expect("Decrypt Failed");
+
+    println!("plain_text:\n{}", String::from_utf8(plain_text).expect("Invalid UTF-8"));
 
 }
