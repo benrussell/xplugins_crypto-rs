f938a9c5dd7b6549ed5130eba4d46c83d916ca1a
Ben Russell <br@x-plugins.com>
2023-05-06 03:31:35 +1000

    Fixed PEM decode bug with \r\n

--- src/pem.rs
+++ src/pem.rs
@@ -3,7 +3,7 @@ use base64::{Engine as _, engine::{general_purpose}};
 
 
 fn strip_pem_header( mut data: Vec<u8> ) -> Vec<u8>{
-    const PEM_HEADER: &str = "-----BEGIN PUBLIC KEY-----\n";
+    const PEM_HEADER: &str = "-----BEGIN PUBLIC KEY-----\r\n";
     data = data.split_off( PEM_HEADER.len() );
 
     data
@@ -11,7 +11,7 @@ fn strip_pem_header( mut data: Vec<u8> ) -> Vec<u8>{
 
 
 fn strip_pem_footer( mut data: Vec<u8> ) -> Vec<u8>{
-    const PEM_HEADER: &str = "-----END PUBLIC KEY-----\n";
+    const PEM_HEADER: &str = "-----END PUBLIC KEY-----\r\n";
     data.truncate(data.len() - PEM_HEADER.len());
 
     data
@@ -29,16 +29,26 @@ pub fn pem_to_der( filename: &str ) -> Result<Vec<u8>, String>{
     let file_read_err = format!("Unable to read source file: {}", filename);
     fh.read_to_end(&mut data).expect(&file_read_err);
     drop(fh);
-    //println!("read {} bytes of pem data.", data.len());
+    println!("read {} bytes of pem data.", data.len());
 
     data = strip_pem_header(data);
     data = strip_pem_footer(data);
 
     let data = String::from_utf8(data).expect("UTF-8 error")
-        .replace("\n","");
+        .replace("\r\n","");
 
-    let bytes = general_purpose::STANDARD
-        .decode(data).unwrap()
+    let bytes = match general_purpose::STANDARD
+            .decode(&data){
+                Ok(bytes) => bytes,
+                Err(msg) => {
+                    //FIXME: Disable data dump on error..
+                    println!("{:?}", data);
+                    println!("");
+                    println!("Base64 Decode Error: {}", msg);
+                    
+                    panic!("{}",msg);
+                }
+        }
         .split_off( 24 );
 
     Ok(bytes)
