ae64ba58f3e1cc08881ebd2634352e49037dfe82
Ben Russell <br@x-plugins.com>
2023-05-06 04:13:56 +1000

    pem.rs: Fixes line ending bug.

--- src/pem.rs
+++ src/pem.rs
@@ -2,23 +2,12 @@
 use base64::{Engine as _, engine::{general_purpose}};
 
 
-fn strip_pem_header( mut data: Vec<u8> ) -> Vec<u8>{
-    const PEM_HEADER: &str = "-----BEGIN PUBLIC KEY-----\r\n";
-    data = data.split_off( PEM_HEADER.len() );
-
-    data
-}
-
-
-fn strip_pem_footer( mut data: Vec<u8> ) -> Vec<u8>{
-    const PEM_HEADER: &str = "-----END PUBLIC KEY-----\r\n";
-    data.truncate(data.len() - PEM_HEADER.len());
-
-    data
-}
-
 
 pub fn pem_to_der( filename: &str ) -> Result<Vec<u8>, String>{
+
+    
+    // git will auto-translate line endings inside PEM files
+    // so we need code to deal with both forms.
 
     use std::io::Read;
 
@@ -31,12 +20,16 @@ pub fn pem_to_der( filename: &str ) -> Result<Vec<u8>, String>{
     drop(fh);
     println!("read {} bytes of pem data.", data.len());
 
-    data = strip_pem_header(data);
-    data = strip_pem_footer(data);
+    let mut data = String::from_utf8(data).expect("UTF-8 error");
+    data = data.replace("\r","");
+    data = data.replace("\n","");
 
-    let data = String::from_utf8(data).expect("UTF-8 error")
-        .replace("\r\n","");
-
+    const PEM_HEADER: &str = "-----BEGIN PUBLIC KEY-----";
+    data = data.replace( PEM_HEADER, "" );
+    
+    const PEM_FOOTER: &str = "-----END PUBLIC KEY-----";
+    data = data.replace( PEM_FOOTER, "" );
+    
     let bytes = match general_purpose::STANDARD
             .decode(&data){
                 Ok(bytes) => bytes,
